9dbab70cc0a240c89fc62a8fd7e1a97304774730
Mikail Kaya <mikailkaya@Mikails-MacBook-Air.local>
2019-11-20 20:41:00 -0500

    Updates

---
+++ start.sh
@@ -0,0 +1,9 @@
+export FLASK_APP=microblog.py
+export FLASK_DEBUG=0
+export MAIL_SERVER=smtp.googlemail.com
+export MAIL_PORT=587
+export MAIL_USE_TLS=1
+export MAIL_USERNAME="mikailkaya38@gmail.com"
+export MAIL_PASSWORD="Mk9765797657!"
+
+flask run
