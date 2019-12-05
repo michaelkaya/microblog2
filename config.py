bade6cbdac2a34b122c47f19cc06a993c0c07458
Mikail Kaya <mikailkaya@Mikails-MacBook-Air.local>
2019-12-05 13:16:31 -0500

    Mikail microblog 2

--- config.py
+++ config.py
@@ -13,3 +13,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['mikailkaya38@gmail.com']
+    POSTS_PER_PAGE = 1
